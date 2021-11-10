f303ef3aa01fdb8d9cd890a606441b3ad43ace71
Wilson <wilson-gs@hotmail.com>
2021-11-09 21:23:35 -0300

    Finalizado aula 142

--- README.md
+++ README.md
@@ -145,6 +145,7 @@ Aulas do curso Maratona Java oferecido gratuitamente pela DevDojo, este reposit�
 139. Classes Utilitárias - IO pt 02 - FileWriter
 140. Classes Utilitárias - IO pt 03 - FileReader
 141. Classes Utilitárias - IO pt 04 - BufferedWriter
+142. Classes Utilitárias - IO pt 05 - BufferedReader
 
 
 
--- src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)) {
-            br.write("O devDojo é lindo, é o melhor curso do Brasillllllll");
-            br.newLine();
-            br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("O devDojo é lindo, é o melhor curso do Brasillllllll");
+            bw.newLine();
+            bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }

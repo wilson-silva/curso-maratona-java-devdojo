0313b8eec6eb466300af660419ab0130f3e5c8f9
Wilson <wilson-gs@hotmail.com>
2022-03-15 07:42:18 -0300

    modificado arquivos

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -8,13 +8,13 @@ public class Aula08ArraysMultidimensionais01 {
         dias[0][1] = 28;
         dias[0][2] = 31;
 
-        dias[1][0] = 31;
-        dias[1][1] = 28;
-        dias[1][2] = 31;
+        dias[1][0] = 25;
+        dias[1][1] = 26;
+        dias[1][2] = 27;
 
-        dias[2][0] = 31;
-        dias[2][1] = 28;
-        dias[2][2] = 31;
+        dias[2][0] = 15;
+        dias[2][1] = 17;
+        dias[2][2] = 18;
 
         for(int i = 0; i < dias.length; i++){
            for (int j = 0; j <dias[i].length; j++ ){
--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -5,6 +5,7 @@ public class Aula08ArraysMultidimensionais02 {
 
         int[]  array = {1, 2, 3};
         int[][] arrayInt = new int[3][];
+
         arrayInt[0] = new int[2];
         arrayInt[1] = array;
         arrayInt[2] = new int[]{1,2,3,4,5,6,7};
